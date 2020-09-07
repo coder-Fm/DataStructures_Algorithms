8563bf349edd1fcbe7026544b747ab609635c765
F471m4 <40557297+fatima-mo@users.noreply.github.com>
2020-09-07 16:13:56 -0700

    Trie.java

--- Trie/Trie.java
+++ Trie/Trie.java
@@ -32,7 +32,7 @@ class Trie {
     /** Inserts a word into the trie. */
     public void insert(String word) {
         TrieNode node = root;
-        for(int i = 0; i < word.length(); i++) {
+        for(int i = 0; i < word.length(); i++) { 
             char currentChar = word.charAt(i);
             if (!node.containsKey(currentChar)) {
                 node.put(currentChar, new TrieNode());
